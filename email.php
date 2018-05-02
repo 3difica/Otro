62ac72d80c415ea13898e6d0fa7faebab6058fcd
damiandpx <32001554+damiandpx@users.noreply.github.com>
2018-05-02 18:28:40 -0500

    Add files via upload

---
+++ email.php
@@ -0,0 +1,44 @@
+﻿<?php
+
+
+
+$nombre = $_POST['nombre'];
+$empresa = $_POST ['empresa'];
+$pais = $_POST ['pais'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$asunto = $_POST['motivo'];
+$mensaje = $_POST['mensaje'];
+
+
+    //Verifica campos vacios
+    if (empty($nombre) or empty($email) or empty($telefono) or empty($mensaje)) {
+            echo "Algunos campos obligados estan vacios."; 
+    }
+
+    else{
+
+        $header = 'From: ' .$email . " \r\n";
+        $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+        $header .= "Mime-Version: 1.0 \r\n";
+        $header .= "Content-Type: text/plain";
+
+        $info = "Este mensaje fue enviado por: "  . $nombre ." \r\n";
+        $info .= "Su empresa es: " . $empresa . " \r\n";
+    	$info .= "Pais: " . $pais . " \r\n";
+        $info .= "Su correo es: " . $email . " \r\n";
+        $info .= "Su teléfono es: " . $telefono . " \r\n";
+        $info .= "Asunto: " . $asunto . " \r\n";
+        $info .= "Mensaje: " . $mensaje . " \r\n";
+        $info .= "Enviado: " . date('d/m/Y', time());
+
+        $para = 'damian@3difica.com';
+        $asunto = 'Mensaje de formulario';
+
+        mail($para, utf8_decode($asunto), utf8_decode($info), $header);
+
+        echo "Su mensaje fue enviado exitosamente. Gracias !";
+    }
+
+
+?>
